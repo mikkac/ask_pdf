1b90c0b030a64620972acfb58f6eafaea61cfadd
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2023-12-02 19:23:16 +0100

    display chat example

--- ask_pdf/app.py
+++ ask_pdf/app.py
@@ -1,9 +1,26 @@
-""" Flashcards generator using LLM as a backend """
-
 import streamlit as st
 
-def main():
-    st.write("Hello world")
+st.title("Ask the PDF")
 
-if __name__ == "__main__":
-    main()
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("Ask a question"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
