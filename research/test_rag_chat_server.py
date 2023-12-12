88d4cf2a4e427dd8bf06e82897bfb0a1cc7c80b4
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2023-12-12 20:30:37 +0100

    made rag chat server test work

--- research/test_rag_chat_server.py
+++ research/test_rag_chat_server.py
@@ -2,10 +2,13 @@ import requests
 
 server_url = "http://127.0.0.1:8000"
 
-pdf_file_path = "path/to/your/file.pdf"
+pdf_file_path = "./1706.03762.pdf"
 
 with open(pdf_file_path, 'rb') as file:
-    response = requests.post(f"{server_url}/create_embeddings", files={"file": file})
+    response = requests.post(
+        f"{server_url}/create_embeddings", 
+        files={"file": ("filename.pdf", file, "application/pdf")}
+    )
     print("Upload Response:", response.json())
 
 def send_message_and_print_response(message):
@@ -14,4 +17,4 @@ def send_message_and_print_response(message):
 
 send_message_and_print_response("What is the main topic of the uploaded document?")
 send_message_and_print_response("Can you summarize the key points?")
-send_message_and_print_response("What are the conclusions drawn in the document?")
+send_message_and_print_response("What is 'transformer' according to the text?")
